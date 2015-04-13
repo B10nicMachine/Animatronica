2c41592e64fa7f0cb6a5ae68ee40f5a35d0598ff
Andrew <Dark32@users.noreply.github.com>
2015-04-13 21:25:09 +0300

    Update GuiPatterns.java

--- java/animatronica/client/gui/GuiPatterns.java
+++ java/animatronica/client/gui/GuiPatterns.java
@@ -7,22 +7,18 @@ import net.minecraft.inventory.Slot;
 import net.minecraft.util.ResourceLocation;
 import animatronica.Animatronica;
 
-public class GuiPatterns extends GuiContainer {
+public class GuiPatterns { //extends GuiContainer { // это нам уже не надо, будет статик хелпером
 	
-	public GuiPatterns(Container container) {
-		super(container);
-	}
+//	public static GuiPatterns(GuiContainer cont, Container container) {
+//		super(container);
+//	}
 
-	public void renderSlot(Slot slot) {
-        int x = (this.width - this.xSize) / 2;
-        int y = (this.height - this.ySize) / 2;
+	public void renderSlot(GuiContainer cont, Slot slot) {
+        int x = (cont.width - cont.xSize) / 2;
+        int y = (cont.height - cont.ySize) / 2;
         ResourceLocation slotTex = new ResourceLocation(Animatronica.MOD_ID.toLowerCase(), "textures/gui/elements/Slot_base.png");
 		Minecraft.getMinecraft().getTextureManager().bindTexture(slotTex);
-		this.drawTexturedModalRect(x+slot.xDisplayPosition-1, y+slot.yDisplayPosition-1, 0, 0, 18, 18);
-	}
-
-	@Override
-	protected void drawGuiContainerBackgroundLayer(float magicFloat, int mouseX, int mouseY) {		
+		cont.drawTexturedModalRect(x+slot.xDisplayPosition-1, y+slot.yDisplayPosition-1, 0, 0, 18, 18);
 	}
 
 }
